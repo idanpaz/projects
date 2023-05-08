f4e8f2b820eff4cc023015f47e012456ae81e1e1
idanpaz <132878773+idanpaz@users.noreply.github.com>
2023-05-08 11:48:41 +0300

    Add files via upload

---
+++ quick_pop_ordered_list.cpp
@@ -0,0 +1,139 @@
+#include <cstddef> // size_t
+#include <iostream> // test
+#include <pthread.h> // mutex 
+
+template <typename T>
+class QuickPopOrderedList
+{
+public:
+    explicit QuickPopOrderedList(int (*cmpFunc)(const T lhs, const T rhs));
+    ~QuickPopOrderedList();
+
+    QuickPopOrderedList(const QuickPopOrderedList& other) = delete;
+    QuickPopOrderedList& operator=(const QuickPopOrderedList& other) = delete;
+    
+    void Push(T data); // might throw bad_alloc
+    T Pop();
+    size_t Size();
+
+protected:
+
+private:
+    struct node
+    {
+        T data;
+        struct node *next;
+    } *m_head;
+
+    int (*m_cmpFunc)(const T lhs, const T rhs);
+    size_t m_size;
+    pthread_mutex_t m_lock;
+};
+
+template <typename T>
+QuickPopOrderedList<T>::QuickPopOrderedList(int (*cmpFunc)(const T lhs, const T rhs)) : 
+m_head(NULL), m_cmpFunc(cmpFunc), m_size(0), m_lock({0}) {}
+
+template <typename T>
+QuickPopOrderedList<T>::~QuickPopOrderedList()
+{
+    struct node *temp = NULL;
+
+    while (m_head != NULL)
+    {
+        temp = m_head;
+        m_head = m_head->next;
+        delete temp;
+    }
+}
+
+template <typename T>
+void QuickPopOrderedList<T>::Push(T data)
+{
+    struct node *new_node = new struct node;
+    struct node *iter = m_head;
+
+    pthread_mutex_lock(&m_lock);
+
+    new_node->data = data;
+    new_node->next = NULL;
+    ++m_size;
+
+    if (!iter)
+    {
+        m_head = new_node;
+        new_node->next = NULL;
+        pthread_mutex_unlock(&m_lock);
+        return;
+    }
+
+    while ((m_cmpFunc(iter->data, data) < 0) && iter->next)
+    {
+        iter = iter->next;
+    }
+
+    if ((m_cmpFunc(iter->data, data) < 0) && !iter->next)
+    {
+        iter->next = new_node;
+        pthread_mutex_unlock(&m_lock);
+        return;
+    }
+
+    new_node->next = iter->next;
+    iter->next = new_node;
+    new_node->data = iter->data;
+    iter->data = data;
+
+    pthread_mutex_unlock(&m_lock);
+}
+
+template <typename T>
+T QuickPopOrderedList<T>::Pop()
+{
+    pthread_mutex_lock(&m_lock);
+    struct node *temp = m_head;
+    T ret = m_head->data;
+
+    --m_size;
+    
+    m_head = m_head->next;
+    delete temp;
+
+    pthread_mutex_unlock(&m_lock);
+    
+    return ret;
+}
+
+template <typename T>
+size_t QuickPopOrderedList<T>::Size()
+{
+    return m_size;
+}
+
+int IntCompareFunc(const int a, const int b)
+{
+    return b - a;
+}
+
+QuickPopOrderedList<int> list(IntCompareFunc);
+
+int main()
+{
+
+    list.Push(5);
+    list.Push(1);
+    list.Push(3);
+    list.Push(2);
+    list.Push(8);
+    list.Push(4);
+    list.Push(6);
+    list.Push(9);
+    list.Push(7);
+
+    while (list.Size())
+    {
+        std::cout << list.Pop() << "\n";
+    }
+
+    return 0;
+}
---
+++ quick_push_ordered_list.cpp
@@ -0,0 +1,151 @@
+#include <cstddef> // size_t
+#include <iostream> // test
+#include <semaphore>
+
+template <typename T>
+class QuickPushOrderedList
+{
+public:
+    explicit QuickPushOrderedList(int (*cmpFunc)(const T lhs, const T rhs));
+    ~QuickPushOrderedList();
+
+    QuickPushOrderedList(const QuickPushOrderedList& other) = delete;
+    QuickPushOrderedList& operator=(const QuickPushOrderedList& other) = delete;
+    
+    void Push(T data); // might throw bad_alloc
+    T Pop();
+    size_t Size();
+
+protected:
+
+private:
+    struct node
+    {
+        T data;
+        struct node *next;
+    } *m_head;
+
+    int (*m_cmpFunc)(const T lhs, const T rhs);
+    size_t m_size;
+};
+
+template <typename T>
+QuickPushOrderedList<T>::QuickPushOrderedList(int (*cmpFunc)(const T lhs, const T rhs)) : 
+m_head(NULL), m_cmpFunc(cmpFunc), m_size(0) {}
+
+template <typename T>
+QuickPushOrderedList<T>::~QuickPushOrderedList()
+{
+    struct node *temp = NULL;
+
+    while (m_head != NULL)
+    {
+        temp = m_head;
+        m_head = m_head->next;
+        delete temp;
+    }
+}
+
+template <typename T>
+void QuickPushOrderedList<T>::Push(T data)
+{
+    struct node *new_node = new struct node;
+    
+    new_node->data = data;
+    new_node->next = m_head;
+    m_head = new_node;
+
+    ++m_size;
+}
+
+template <typename T>
+T QuickPushOrderedList<T>::Pop()
+{
+    struct node *retNode = m_head;
+    struct node *temp = m_head->next;
+    T ret;
+
+    while (temp != NULL)
+    {
+        if (m_cmpFunc(retNode->data, temp->data) > 0)
+        {
+            retNode = temp;
+        }
+
+        temp = temp->next;
+    }
+
+    ret = retNode->data;
+    temp = retNode->next;
+
+    if (temp)
+    {
+        retNode->data = temp->data;
+        retNode->next = temp->next;
+
+        if (temp == m_head)
+        {
+            m_head = temp->next;
+        }
+
+        delete temp; temp = NULL;
+    }
+
+    else
+    {
+        temp = m_head;
+        while (temp && temp->next != retNode)
+        {
+            temp = temp->next;
+        }
+
+        if (temp)
+        {
+            if (retNode == m_head)
+            {
+                m_head = retNode->next;
+            }
+
+            delete retNode; retNode = NULL;
+            temp->next = NULL;
+        }
+    }
+
+    --m_size;
+    
+    return ret;
+}
+
+template <typename T>
+size_t QuickPushOrderedList<T>::Size()
+{
+    return m_size;
+}
+
+int IntCompareFunc(const int a, const int b)
+{
+    return b - a;
+}
+
+int main()
+{
+    QuickPushOrderedList<int> list(IntCompareFunc);
+
+    list.Push(5);
+    list.Push(1);
+    list.Push(3);
+    list.Push(2);
+    list.Push(8);
+    list.Push(4);
+    list.Push(6);
+    list.Push(9);
+    list.Push(7);
+
+    while (list.Size())
+    {
+        std::cout << list.Pop() << "\n";
+    }
+
+    
+    return 0;
+}
